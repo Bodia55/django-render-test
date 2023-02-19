1b585d5bb4ddac10896f8e93a4adddf6274da44a
Bodia55 <85449203+Bodia55@users.noreply.github.com>
2023-02-19 18:16:16 +0200

    Add files via upload

---
+++ products/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+import uuid
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
